b2e8bb41342e241c0551b825294d4d091340a4f6
YahyaIqablani <yahya.iqablani@itis.biella.it>
2023-05-10 12:40:22 +0200

    primo punto semaforo rosso

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EducazioneCivica
+{
+    class Sicurezza
+    {
+         static void Main(string[] args)
+        {
+             int restart = 0;
+            do{
+                Console.Write("Inserisci una stringa: ");
+                string s = Console.ReadLine();
+                Console.Write("Inserisci un numero: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write("Che funzione vuoi eseguire? ");
+                int funz=int.Parse(Console.ReadLine());
+                switch(funz)
+                {
+                    case 1: PrimoPunto(s, n);
+                    break;
+                    
+                    
+                }
+                
+
+
+                Console.Write("Rieseguire? 1/0: ");
+                restart = int.Parse(Console.ReadLine());
+            }while (restart == 1);
+            
+        
+        }
+        
+        static string s ="";
+        static int n = 0;
+        
+        
+
+
+        
+        
+        public static void PrimoPunto(string s,int n)
+        {
+            return false;
+                
+        }
+       
+
+
+               
+
+
+            }
+}
+        
+    
